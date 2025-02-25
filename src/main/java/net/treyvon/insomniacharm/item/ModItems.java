14fbc8e7888ca50092a0306ef9b1c2d6d3af394f
TreyM05 <Treymiller2205@gmail.com>
2025-02-25 13:52:31 -0500

    Removed the Fragment of Insomnia

--- src/main/java/net/treyvon/insomniacharm/item/ModItems.java
+++ src/main/java/net/treyvon/insomniacharm/item/ModItems.java
@@ -11,8 +11,6 @@ import top.theillusivec4.curios.api.CuriosApi;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(InsomniaCharm.MOD_ID);
 
-    public static final DeferredItem<Item> INSOMNIACHARM = ITEMS.register("insomniashard", () -> new Item(new Item.Properties()));
-
     public static final DeferredItem<Item> CHARMINSOMNIA = ITEMS.register("insomniacharm",
             () -> new CharmItem(new Item.Properties().stacksTo(1)));
 
